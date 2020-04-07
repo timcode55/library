111bc188a85036ee39140cb1b2832b9fcc07d2cc
Tim Petrvalsky <tpet55@yahoo.com>
2020-04-07 13:25:50 -0700

    app is working fully

--- app.js
+++ app.js
@@ -1,24 +1,5 @@
 let myLibrary = [];
 
-let books = [
-	{
-		title: 'How to win friends and influence people',
-		author: 'dale carnegie',
-		pages: 450,
-		cover: 'img',
-		isRead: false
-	},
-	{
-		title: 'Unlimited Power',
-		author: 'Tony Robbins',
-		pages: 600,
-		cover: 'img',
-		isRead: true
-	}
-];
-
-// console.log(books[0].title);
-
 function Book(title, author, pages, cover, isRead) {
 	this.title = title;
 	this.author = author;
@@ -35,36 +16,62 @@ function addBookToLibrary(obj) {
 	render();
 }
 
-function addBookToLibrary() {
-	// do stuff here
+// Display entered book info onto page
+
+function displayBook() {
+	let titleInput = document.getElementById('title');
+	let authorInput = document.getElementById('author');
+	let imageInput = document.getElementById('image');
+	let beenRead = document.getElementById('is-read');
+	document.querySelector('form.book-form').addEventListener('submit', function(e) {
+		e.preventDefault();
+		let h1 = document.createElement('h1');
+		let h2 = document.createElement('h2');
+		let h3 = document.createElement('h3');
+		let h3delete = document.createElement('button');
+		let imgDiv = document.createElement('img');
+		let section = document.createElement('div');
+		let addItem = document.querySelector('.library');
+		h1.textContent = titleInput.value;
+		h2.textContent = authorInput.value;
+
+		h3.innerHTML = `<label class="checked-read">Read
+      <input type="checkbox" checked="checked">
+      <span class="checkmark"></span>
+    </label>`;
+
+		// let selectRead = document.querySelector('.custom-select');
+
+		imgDiv.src = `${imageInput.value}`;
+		imgDiv.width = 200;
+		h3delete.textContent = 'Delete';
+		h3delete.classList.add('book-delete');
+
+		addItem.appendChild(section);
+		section.classList.add('current-div');
+		section.appendChild(h1);
+
+		section.appendChild(h2);
+		section.appendChild(imgDiv);
+		section.appendChild(h3);
+		section.appendChild(h3delete);
+		titleInput.value = '';
+		authorInput.value = '';
+		imageInput.value = '';
+		deleteBook();
+	});
 }
 
-let titleInput = document.getElementById('title');
-let authorInput = document.getElementById('author');
-let imageInput = document.getElementById('image');
-document.querySelector('form.book-form').addEventListener('submit', function(e) {
-	e.preventDefault();
+// Delete book from display
 
-	// console.log(titleInput.value);
-	// console.log(authorInput.value);
-	// console.log(imageInput.value);
-	let h1 = document.createElement('h1');
-	let h2 = document.createElement('h2');
-	let imgDiv = document.createElement('img');
-	let section = document.createElement('div');
-	let addItem = document.querySelector('.library');
-	// h1.textContent = printbooks()[0];
-	h1.textContent = titleInput.value;
-	h2.textContent = authorInput.value;
-	imgDiv.src = `${imageInput.value}`;
-	imgDiv.width = 200;
+function deleteBook() {
+	let deleteBtn = document.querySelectorAll('.book-delete');
+	deleteBtn.forEach((button) => {
+		button.addEventListener('click', function(e) {
+			console.log(e.target.parentElement.parentElement);
+			e.target.parentElement.remove();
+		});
+	});
+}
 
-	addItem.appendChild(section);
-	section.appendChild(h1);
-
-	section.appendChild(h2);
-	section.appendChild(imgDiv);
-	titleInput.value = '';
-	authorInput.value = '';
-	imageInput.value = '';
-});
+displayBook();
